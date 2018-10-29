04dd1bfbcc5cfdc75afaf66a0fd80333e3be9a01
Jonathan Amsterdam <jba@google.com>
2018-10-29 18:58:15 -0400

    with testmod1@v0.0.3

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jba/testmod-client
+
+require github.com/jba/testmod1 v0.0.3
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jba/testmod1/blue"
+	"github.com/jba/testmod1/purple"
+	"github.com/jba/testmod1/red"
+)
+
+func main() {
+	fmt.Println(red.Name)
+	fmt.Println(blue.Name)
+	fmt.Println(purple.Name)
+}
